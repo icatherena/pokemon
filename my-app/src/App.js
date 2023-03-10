7c11e0f8f921f6a3a9bd91c3cf8b82cf36ee5814
icatherena <beresovskyc@gmail.com>
2023-03-10 07:32:40 -0300

    creación react-app + arrancando con componentes mui

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Card from './components/Card';
+import { Grid } from '@mui/material';
+
+function App() {
+  return (
+    <Grid>
+      <Card/>
+    </Grid>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/Card.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Grid } from '@mui/material';
+
+function Card() {
+    return (
+        <Grid sx={{backgroundColor: 'red'}}>
+            <h1>Componente card</h1>
+        </Grid>
+    )
+}
+
+export default Card;
